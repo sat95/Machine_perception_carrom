d12550bbbb8cf27442e69b480a748438abe09849
sat95 <panda.sathyam9@gmail.com>
2017-04-11 11:14:18 +0530

    v1.0

---
+++ learn.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void src_loc_set(char *src_loc,int num,char *arg)
+{
+	char buffer[5];
+	sprintf(buffer,"%d",num);
+	strcpy(src_loc,"dataset/");
+	strcat(src_loc,arg);
+	strcat(src_loc,buffer);
+	strcat(src_loc,".jpg");
+}
+void dst_loc_set(char *dst_loc,int num1, int num2,char *arg)
+{
+	char buffer[5];
+	sprintf(buffer,"%d",num1);
+	strcpy(dst_loc,buffer);
+	strcat(dst_loc,"/");
+	sprintf(buffer,"%d",num2);
+	strcat(dst_loc,arg);
+	strcat(dst_loc,buffer);
+	strcat(dst_loc,".jpg");
+}
+void execute_cmd(char *cmd ,char *src_loc, char *dst_loc)
+{
+	strcpy(cmd,"cp ");
+	strcat(cmd,src_loc);
+	strcat(cmd," ");
+	strcat(cmd,dst_loc);
+	//printf("Executing %s\n",cmd);
+	system(cmd);
+}
+int main(int argc, char **argv)
+{
+	FILE *fp = fopen("data.txt","r+"),*src,*dst;
+	double pr[11]={0};
+	int i,j,num,diff=0;
+	char res,*src_loc,*dst_loc,*command;
+	src_loc = (char *)malloc(20);
+	dst_loc = (char *)malloc(20);
+	command = (char *)malloc(50);
+	do{
+		diff++;
+		for(i=1;i<=11;i++)
+		{
+			for(j=1;j<=10;j++){
+				fscanf(fp,"%d %c",&num,&res);
+				if(res=='h') pr[i-1]++;
+			}
+		}
+	}while(fscanf(fp,"%c",&res)!=EOF);
+	fclose(fp);
+	for(i=0;i<11;i++){
+		pr[i]=(pr[i]/(diff*10))*100;
+		printf("P(%d) = %.1f Aprrox = %.1f\n",i+1,pr[i],ceil(pr[i]));
+		pr[i] = ceil(pr[i]);
+	}
+	for(i=0;i<11;i++){
+		for(j=10;j<=100;j=j+10){
+			if(!((int)pr[i]/j)){printf("Class(%d) -> %d%%\n",i+1,j); break;}
+		}
+		src_loc_set(src_loc,i+1,argv[1]);
+		dst_loc_set(dst_loc,j,i+1,argv[1]);
+		//printf("src_loc = %s\ndst_loc = %s\n",src_loc,dst_loc);
+		execute_cmd(command,src_loc,dst_loc);
+	}
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <limits.h>
+
+#define sq(x) (x*x)
+#define CLASSES 10
+typedef struct image{
+	int pos;
+	char path[20];
+	int c[2];
+	double mag;
+}img;
+
+void insert_sort(img *arr,int n)
+{
+	img temp;
+	int i,j,k;
+	for(i=1;i<n;i++)
+	{
+		temp = arr[i];
+		for(j=i-1;j>=0;j--)
+		{
+			if(arr[j].mag>temp.mag){
+				arr[j].pos = j+1;
+				arr[j+1]=arr[j]; 
+			}
+			else break;
+		}
+		arr[j+1]=temp;
+		arr[j+1].pos = j+1;
+	}
+}
+void sort_database(img **database,int *size)
+{
+	int i,j;
+	for(i=0;i<CLASSES;i++)
+		if(size[i])
+			insert_sort(database[i],size[i]);
+}
+void print_database(img **database,int *size)
+{
+	int i,j;
+	for(i=0;i<CLASSES;i++)
+	{
+		if(size[i])
+			printf("(%d)\n",i+1);
+		for(j=0;j<size[i];j++)
+		{
+			printf("(%d) %s -> %d %d %.2f\n"
+				,database[i][j].pos
+				,database[i][j].path
+				,database[i][j].c[0]
+				,database[i][j].c[1]
+				,database[i][j].mag);
+		}
+	}
+}
+void create_database(FILE *fp,img **database,int *size)
+{
+	int i,j,k=0,num;
+	while(fscanf(fp,"%d ",&num)!=EOF)
+	{
+		size[k]=num;
+		if(size[k]){
+			database[k] = (img *)malloc(num*sizeof(img));
+			for(i=0;i<num;i++)
+			{
+				fscanf(fp,"%[^\n]",database[k][i].path);
+				fscanf(fp,"%d %d ",
+					database[k][i].c+0,
+					database[k][i].c+1);
+					database[k][i].pos=i;
+					database[k][i].mag=sqrt(sq(database[k][i].c[0])+sq(database[k][i].c[1])); 
+			}
+			k++;
+		}
+	}
+}
+img *max(img *arr, int first, int second, double i_mag)
+{
+	double one = abs(arr[first].mag-i_mag);
+	double two = abs(arr[second].mag-i_mag);
+	if(one>=two) return (arr+second);
+	else return (arr+first);
+}
+img *search_each(img *arr, int start, int end, int pa_size, double i_mag,int num)
+{
+	//printf("At start=%d end =%d pa_size=%d num=%d\n",start,end,pa_size,num);
+	int mid=(start+end)/2;
+	if(start>=end)
+	{
+		//printf("At strat=end\n");
+		if(i_mag>arr[start].mag)
+		{
+			if(start+1<=pa_size) return max(arr,start,start+1,i_mag);
+			else return (arr+start);
+		}
+		else if(i_mag<arr[start].mag)
+		{
+			if(start-1>=0) return max(arr,start,start-1,i_mag);
+			else return (arr+start);
+		}
+		else return (arr+start);
+	}
+	else
+	{
+		if(i_mag>arr[mid].mag) 
+			return search_each(arr,mid+1,end,pa_size,i_mag,num);
+		else if(i_mag<arr[mid].mag) 
+			return search_each(arr,start,mid-1,pa_size,i_mag,num);
+		else
+			return (arr+mid);
+	}
+}
+img *search_database(img **database,int *size, img *input, img *output)
+{
+	int i,k;
+	double i_mag = input->mag;
+	double eucl_dist,dist,dist_l,dist_r,min_dist=INT_MAX;
+	img *prev,*curr,*left,*right,*temp;
+	for(i=0;i<CLASSES;i++)
+		if(size[i]){
+			k=1;
+			prev = search_each(database[i],0,size[i]-1,size[i]-1,i_mag,i);
+			eucl_dist = sqrt(pow(prev->c[0]-input->c[0],2)+pow(prev->c[1]-input->c[1],2));
+			dist = eucl_dist;
+			//printf("eucl_dist=%.2f for %s\n",eucl_dist,prev->path);
+			curr = prev;
+			temp = prev;
+			do{
+				prev = temp;
+				if(curr->pos-k>=0){
+					//printf("curr->pos-k>=0 k=%d %d>=%d\n",k,curr->pos-k,0);
+					left = curr-k;
+					dist_l = sqrt(pow(left->c[0]-input->c[0],2)+pow(left->c[1]-input->c[1],2));
+					//printf("dist_l=%.2f\n",dist_l);	
+				}
+				if(curr->pos+k<size[i]){
+					//printf("curr->pos+k<size[i] k=%d %d<%d\n",k,curr->pos+k,size[i]);
+					right = curr+k;
+					dist_r = sqrt(pow(right->c[0]-input->c[0],2)+pow(right->c[1]-input->c[1],2));
+					//printf("dist_r=%.2f\n",dist_r);
+				}
+				if(curr->pos-k<0||curr->pos+k>=size[i]){
+					
+					if(curr->pos-k<0&&curr->pos+k>=size[i]) break;
+					else if(curr->pos-k<0) {dist = dist_r; temp = right;}
+					else if(curr->pos+k>=size[i]) {dist = dist_l; temp = left;}
+				}
+				else if(dist_l<=dist_r) {dist = dist_l; temp = left;}
+				else if(dist_l>dist_r) {dist = dist_r; temp = right;}
+				k++;
+			}while(dist<=eucl_dist);
+			if(abs(prev->mag-i_mag)<=abs(output->mag-i_mag)&&min_dist>=dist){
+				min_dist=dist;
+				output = prev;
+			} 
+		}
+	return output;
+}
+int main(int argc, char **argv)
+{
+	int size[10]={0},i_x,i_y;
+	double i_mag;
+	img **database,*output,*input;
+	database = (img **)malloc(CLASSES*sizeof(img *));
+	output = (img *)calloc(1,sizeof(img));
+	input = (img *)calloc(1,sizeof(img));
+	output->mag=0;
+	FILE *fp = fopen("img_database.txt","r");
+	create_database(fp,database,size);
+	fclose(fp);
+	sort_database(database,size);
+	print_database(database,size);
+	printf("\nEnter Input X and Y coordinates\n");
+	scanf("%d %d",input->c+0,input->c+1);
+	input->mag = sqrt(sq(input->c[0])+sq(input->c[1]));
+	printf("\nInput Magnitude %.2f\n",input->mag);
+	output = search_database(database,size,input,output);
+	printf("\nOutput:\nCoin position: %d %d\nMagnitude: %.2f\n",output->c[0],output->c[1],output->mag);
+	printf("Image File: %s\n",output->path);
+	return 0;
+}
