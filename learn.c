fc41284e87c6b2aaf29250195a87df7fbcda88dc
sat95 <panda.sathyam9@gmail.com>
2017-04-12 19:18:40 +0530

    Modified Result

--- learn.c
+++ learn.c
@@ -38,7 +38,8 @@ int main(int argc, char **argv)
 	char *data = (char *)malloc(20);
 	strcpy(data,argv[1]);
 	strcat(data,"data.txt");
-	FILE *fp = fopen(data,"r+"),*src,*dst;
+	FILE *fp = fopen(data,"r+"),*src,*dst,*result;
+	result = fopen("result.txt","a+");
 	double pr[11]={0};
 	int i,j,num,diff=0;
 	char res,*src_loc,*dst_loc,*command;
@@ -56,19 +57,22 @@ int main(int argc, char **argv)
 		}
 	}while(fscanf(fp,"%c",&res)!=EOF);
 	fclose(fp);
+	fprintf(result,"Coin Position :%s\n",argv[1]);
 	for(i=0;i<11;i++){
 		pr[i]=(pr[i]/(diff*10))*100;
-		printf("P(%d) = %.1f Aprrox = %.1f\n",i+1,pr[i],ceil(pr[i]));
+		fprintf(result,"P(%d) = %.1f Aprrox = %.1f\n",i+1,pr[i],ceil(pr[i]));
 		pr[i] = ceil(pr[i]);
 	}
 	for(i=0;i<11;i++){
 		for(j=10;j<=100;j=j+10){
-			if(!((int)pr[i]/j)){printf("Class(%d) -> %d%%\n",i+1,j); break;}
+			if(!((int)pr[i]/j)){fprintf(result,"Class(%d) -> %d%%\n",i+1,j); break;}
 		}
 		src_loc_set(src_loc,i+1,argv[1]);
 		dst_loc_set(dst_loc,j,i+1,argv[1]);
 		//printf("src_loc = %s\ndst_loc = %s\n",src_loc,dst_loc);
 		execute_cmd(command,src_loc,dst_loc);
 	}
+	fprintf(result,"\n");
+	fclose(result);
 	return 0;
 }
